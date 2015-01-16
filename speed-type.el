d22f596ccdc8aa0ca26773c168fdb896c8ef5c90
Gunther Hagleitner <gunther@apache.org>
2015-01-16 00:58:25 -0800

    change header

--- speed-type.el
+++ speed-type.el
@@ -25,9 +25,10 @@
 
 ;;; Commentary:
 ;;
-;; This allows you to practice your touch typing skills on any buffer
-;; or region. You can also type random samples from ebooks. speed-type
-;; also keeps track of your stats (WPM, CPM, etc)
+;; Speed-type allows you to practice your touch typing skills. You can
+;; test yourself by typing snippets from online books or use any piece
+;; of text or code you have in emacs. Speed-type keeps track of your
+;; stats (WPM, CPM, accuracy) while you are typing.
 
 ;;; Code:
 
